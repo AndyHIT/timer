8e4be9323ccc6106e63b643cfc42dec893475d0a
Chenyang Cui <chenyangcui@MacBook-Pro.attlocal.net>
2021-03-21 15:18:24 -0500

    Hours working, added styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Button } from '@material-ui/core';
 import './App.scss';
 
 const App = () => {
@@ -14,14 +15,21 @@ const App = () => {
 
   useEffect(() => {
     let timerInterval = null;
-    if (timerStatus === 'Stop') {
+    if (timerStatus === 'Pause') {
       timerInterval = setInterval(() => {
         if (second > 0) {
           setSecond(second => second - 1);
         }
         if (second === 0) {
           if (minute === 0) {
-            clearInterval(timerInterval);
+            if (hour === 0) {
+              clearInterval(timerInterval);
+            }
+            if (hour > 0) {
+              setHour(hour - 1);
+              setMinute(59);
+              setSecond(59);
+            }
           } 
           if (minute > 0) {
             setMinute(minute - 1);
@@ -37,14 +45,14 @@ const App = () => {
 
   const handleTimer = () => {
     if (timerStatus === 'Start') {
-      setTimerStatus('Stop');
+      setTimerStatus('Pause');
     } else {
       setTimerStatus('Start');
     }
   }
 
   const handleResetTimer = () => {
-    setTimerStatus('Stop');
+    setTimerStatus('Start');
     setHour(0);
     setMinute(0);
     setSecond(0);
@@ -75,38 +83,47 @@ const App = () => {
 
   return (
     <div className="App">
-      <>
+      <div className='set-timer-panel'>
         <input 
           type='number' 
+          min='0'
           id='timer-hr-input' 
           className='timer-section'
           value={initialHour}
           onChange={e => handleChange(e, 'hour')}
         />
-        <label htmlFor='timer-hr-input'>hr</label>
+        <label htmlFor='timer-hr-input'>h</label>
         <input 
           type='number' 
+          min='0'
+          max='59'
           id='timer-min-input' 
           className='timer-section' 
           value={initialMinute}
           onChange={e => handleChange(e, 'minute')}
         />
-        <label htmlFor='timer-min-input'>min</label>
+        <label htmlFor='timer-min-input'>m</label>
         <input 
           type='number'
+          min='0'
+          max='59'
           id='timer-sec-input' 
           className='timer-section' 
           value={initialSecond}
           onChange={e => handleChange(e, 'second')}
         />
-        <label htmlFor='timer-sec-input'>sec</label>
-        <button type='button' onClick={handleTimer}>{timerStatus}</button>
-        <button type='button' onClick={handleResetTimer}>Reset</button>
-      </>
+        <label htmlFor='timer-sec-input'>s</label>
+        <div className='button-panel'>
+          <Button variant="contained" color="primary" className='primary' onClick={handleTimer}>{timerStatus}</Button>
+          <Button variant="contained" color="second" className='second' onClick={handleResetTimer}>Reset</Button>
+        </div>
+      </div>
       <div className='countdown-timer'>
-        <span>{hour}</span>hr
-        <span>{minute}</span>min
-        <span>{second}</span>sec
+        <span className='countdown-number'>{hour < 10 ? `0${hour}` : hour}</span>
+        <span className='number-divider'>:</span>
+        <span className='countdown-number'>{minute < 10 ? `0${minute}` : minute}</span>
+        <span className='number-divider'>:</span>
+        <span className='countdown-number'>{second < 10 ? `0${second}` : second}</span>
       </div>
     </div>
   );
